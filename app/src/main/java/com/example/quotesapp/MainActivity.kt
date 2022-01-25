7cd96e4647f5e8c00f035859d902d0d558f354ca
Ali__Shiri <alishiri067@gmail.com>
2022-01-25 07:02:06 -0800

    add navigation bottom

--- app/src/main/java/com/example/quotesapp/MainActivity.kt
+++ app/src/main/java/com/example/quotesapp/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.example.quotesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.quotesapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationButton: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpBottomNavigationBar()
+    }
+
+    private fun setUpBottomNavigationBar() {
+
+        val navController = Navigation.findNavController(this, R.id.newsNavHostFragment)
+        navigationButton = binding.bottomNavigationView
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(navigationButton, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.newsNavHostFragment)
+
+        return navController.navigateUp() or super.onSupportNavigateUp()
     }
 }
